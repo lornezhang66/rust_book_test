304a664fa7edacca132b5515f9f754a58c99b136
zhangxiaolong <zhangxiaolong@jiuqi.com.cn>
2022-09-21 22:18:59 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use rand::Rng;
+
 fn main() {
     println!("Hello, world!");
+    let rng:u32 = rand::thread_rng().gen_range(0..=100);
+    another_function(rng);
+}
+
+
+fn another_function(i: u32){
+
+    println!("This function number is {i}")
 }
